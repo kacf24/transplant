636e1f8d30b99142164dc6f0130a091825c09a64
kacf24 <kyle.capistrantfossa@maine.edu>
2019-01-25 16:09:41 -0500

    Finished

--- ClusterAnalysis.R
+++ ClusterAnalysis.R
@@ -71,29 +71,27 @@ text.pvclust <- function(x, col=c("gray0","gray50",8), print.num=FALSE,  float=0
 
 #Trim to correct length
 rm(Map.csv)
-AHiC <- AHiC[-1,]
-AMidC <- AMidC[-2038,]
-AMidE <- AMidE[-2038,]
-BHiC <- BHiC[-2038,]
-BHiE <- BHiE[-2038,]
-BMidC <- BMidC[-1,]
-BMidE <- BMidE[-1,]
-
+AHiC.csv <- AHiC.csv[-1,]
+AMidC.csv <- AMidC.csv[-2038,]
+AMidE.csv <- AMidE.csv[-2038,]
+BHiC.csv <- BHiC.csv[-2038,]
+BHiE.csv <- BHiE.csv[-2038,]
+BMidC.csv <- BMidC.csv[-1,]
+BMidE.csv <- BMidE.csv[-1,]
+wind.csv <- wind.csv[-1,]
 #Z-ScoreNormalize
 
-BHiC$Value<-(BHiC$Value - mean(BHiC$Value)) / sd(BHiC$Value)
-BHiE$Value<-(BHiE$Value - mean(BHiE$Value)) / sd(BHiE$Value)
-BMidC$Value<-(BMidC$Value - mean(BMidC$Value)) / sd(BMidC$Value)
-BMidE$Value<-(BMidE$Value - mean(BMidE$Value)) / sd(BMidE$Value)
-AHiC$Value<-(AHiC$Value - mean(AHiC$Value)) / sd(AHiC$Value)
-AMidC$Value<-(AMidC$Value - mean(AMidC$Value)) / sd(AMidC$Value)
-AMidE$Value<-(AMidE$Value - mean(AMidE$Value)) / sd(AMidE$Value)
-wind$X17.2236 <- (wind$X17.2236 - mean(wind$X17.2236)) / sd(wind$X17.2236)
+BHiC<-(BHiC.csv$Value - mean(BHiC.csv$Value)) / sd(BHiC.csv$Value)
+BHiE<-(BHiE.csv$Value - mean(BHiE.csv$Value)) / sd(BHiE.csv$Value)
+BMidC<-(BMidC.csv$Value - mean(BMidC.csv$Value)) / sd(BMidC.csv$Value)
+BMidE<-(BMidE.csv$Value - mean(BMidE.csv$Value)) / sd(BMidE.csv$Value)
+AHiC<-(AHiC.csv$Value - mean(AHiC.csv$Value)) / sd(AHiC.csv$Value)
+AMidC<-(AMidC.csv$Value - mean(AMidC.csv$Value)) / sd(AMidC.csv$Value)
+AMidE<-(AMidE.csv$Value - mean(AMidE.csv$Value)) / sd(AMidE.csv$Value)
+wind<- (wind.csv$Temp - mean(wind.csv$Temp)) / sd(wind.csv$Temp)
 
 #CreateTheTree
-CombinedFrame <- data.frame(A.High.C = AHiC$Value,A.Mid.C = AMidC$Value,A.Mid.E = AMidE$Value,B.High.C = BHiC$Value,B.High.E = BHiE$Value,B.Mid.C = BMidC$Value,B.Mid.E = BMidE$Value, CAMNET.Station = wind$X17.2236)
-IP.dis <- diss(CombinedFrame, "EUCL")
-plot((hclust(IP.dis, method ="ward.D2")))
+CombinedFrame <- data.frame(A.High.C = AHiC,A.Mid.C = AMidC,A.Mid.E = AMidE,B.High.C = BHiC,B.High.E = BHiE,B.Mid.C = BMidC,B.Mid.E = BMidE, CAMNET.Station = wind)
 
 fit <- pvclust(CombinedFrame, method.hclust="ward.D2",method.dist="euclidean")
 plot(fit,main = "",xlab = '', sub = '', col.pv = c("gray","gray0"), float = 0.025, print.num = F)
